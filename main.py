b0e02edfbc34660b0e828e28563b4df62b898fb0
Gage Atkinson <73555272+someStranger8@users.noreply.github.com>
2022-01-10 16:46:34 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
+
 import twint
+
+# banner
 print("")
 print(" |_   _|_ __ _(_) |_| |_ ___ _ _   / _ \/ __|_ _| \| |_   _|")
 print("   | | \ V  V / |  _|  _/ -_) '_| | (_) \__ \| || .` | | |  ")
 print("   |_|  \_/\_/|_|\__|\__\___|_|    \___/|___/___|_|\_| |_|  ")
 print("")
-to=input("[*] enter username: ")
-kw=input("[*] enter keyword: ")
+
+# varibles
+to = sys.argv[1]
+kw = sys.argv[2]
+
+# search
 c = twint.Config()
 c.Username = to
 c.Search = kw
